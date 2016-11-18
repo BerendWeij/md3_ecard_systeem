bd6d5157f19b4b8858a28f999bffa5bbd6fc9354
stijnana <stijn.olthof@live.nl>
2016-11-18 14:18:10 +0100

    split css to scss file

--- client/main.js
+++ client/main.js
@@ -12,14 +12,14 @@ export default class Main {
     this.renderSystem = new RenderSystem();
 
 
-    let test = {x:50, y:-50, z:5, id:"test"};
-    this.renderSystem.add(test);
-    this.renderSystem.add({x:15, y:-150, z:-205, id:"test"});
-    this.renderSystem.add({x:500, y:5, z:5, id:"test"});
-    this.renderSystem.add({x:5, y:-250, z:50, id:"test"});
+    let tree = {x:50, y:-50, z:5, id:"tree"};
+    this.renderSystem.add(tree);
+    this.renderSystem.add({x:15, y:-150, z:-205, id:"car"});
+    this.renderSystem.add({x:500, y:5, z:5, id:"tree"});
+    this.renderSystem.add({x:5, y:-250, z:50, id:"tree"});
 
     setInterval(()=>{
-      test.x += 5;
+      tree.x += 5;
     }, 50);
   }
 
