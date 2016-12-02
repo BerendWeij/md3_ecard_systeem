1ff317c1a0c31b8158303de42b32daafebace0fa
sirbulters <j_bulters@live.no>
2016-12-02 16:26:41 +0100

    started with fetch/post of data

--- client/app/controller/SendMenuController.js
+++ client/app/controller/SendMenuController.js
@@ -5,4 +5,65 @@ export default class SendMenuController {
       sendMenu.classList.toggle("fullHeight");
     });
   }
+
+  sendEcard(){
+    let ecard = {
+      // name: "Luuk",
+      // answer: document.getElementById('answer').value
+
+      creatorName: req.body.creatorName,
+      receiverName: req.body.receiverName,
+      email: req.body.email,
+      objects: [
+        {
+          id: req.body.id,
+          x: req.body.x,
+          y: req.body.y,
+          z: req.body.z,
+          rotation: req.body.rotation,
+          scale: req.body.scale
+        }
+      ]
+
+      // orgineel
+      
+      // creatorName: req.body.creatorName,
+      // receiverName: req.body.receiverName,
+      // email: req.body.email,
+      // objects: [
+      //   {
+      //     id: req.body.id,
+      //     x: req.body.x,
+      //     y: req.body.y,
+      //     z: req.body.z,
+      //     rotation: req.body.rotation,
+      //     scale: req.body.scale
+      //   }
+      // ]
+
+      //test gegevens
+
+      // creatorName: testpersoon,
+      // receiverName: antvangtestpersoon,
+      // email: 'test@test.nl',
+      // objects: [
+      //   {
+      //     id: req.body.id,
+      //     x: req.body.x,
+      //     y: req.body.y,
+      //     z: req.body.z,
+      //     rotation: req.body.rotation,
+      //     scale: req.body.scale
+      //   }
+      // ]
+    };
+
+    let postData = {
+      method: 'post',
+      body: JSON.stringify(ecard)
+    };
+
+    // fetch('https://davidwalsh.name/submit-json', postData);
+    fetch('/ecard', postData);
+  }
 }
